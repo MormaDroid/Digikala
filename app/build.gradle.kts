b86f8406da42748faa981a4049fb6150b311c37f
MormaDroid <mohsen.morma@gmail.com>
2024-01-02 15:53:45 +0330

    Add Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -66,4 +68,39 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
+
+    //room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    ksp ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    //datastore
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+
+    //hilt di
+    implementation ("com.google.dagger:hilt-android:2.50")
+    ksp ("com.google.dagger:hilt-compiler:2.50")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    //compose navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
+
+    //animation
+    implementation ("com.airbnb.android:lottie-compose:6.3.0")
+
+    //coil - load image from url
+    implementation ("io.coil-kt:coil-compose:2.5.0")
+
+    //swipe refresh
+    implementation ("com.google.accompanist:accompanist-swiperefresh:0.32.0")
+
+    //system ui controller
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
+
+
 }
